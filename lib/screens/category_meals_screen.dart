0c59876b68fdb9c84fade701347d1e3c10b8a244
silveiraluisf <silveiraluis88@gmail.com>
2022-10-04 10:41:17 -0300

    navegação retornado categorias específicas

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -10,7 +10,7 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('aaa'),
+          title: Text(categoryTitle),
         ),
         body: Text('As receitas'));
   }
